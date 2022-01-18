c27ac4dab9a12bb0e223a774deed2a82fc82aa81
Jayson Byrd <jayson_byrd_95@outlook.com>
2022-01-18 15:30:09 -0500

    Modal Completed

--- api/cars/cars-model.js
+++ api/cars/cars-model.js
@@ -1,11 +1,15 @@
+const knex = require('knex')
+const db = require('../../data/db-config')
+
 const getAll = () => {
-  // DO YOUR MAGIC
+  return db('cars');
 }
 
-const getById = () => {
-  // DO YOUR MAGIC
+const getById = (id) => {
+  return db('cars').where('id', id).first();
 }
 
-const create = () => {
-  // DO YOUR MAGIC
+const create = (car) => {
+  const [id] = await db('cars').insert(car)
+  return getById(id)
 }
--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -1,1 +1,16 @@
-// DO YOUR MAGIC
+const router = require('express').Router()
+const Cars = require('./cars-model')
+
+router.get('/', (req, res) => {
+    Cars.getAll()
+        .then(cars => {
+            res.json(cars)
+        })
+        .catch(err => {
+            res.status(500).json({message: 'Internal Server Error'})
+        })
+})
+
+
+
+module.exports = router
